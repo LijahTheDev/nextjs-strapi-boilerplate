f61196e38fe2d929a6e77648f78e7359804e56e3
Paul <lijahthedev@gmail.com>
2021-01-04 18:45:47 -0600

    fixed db host for specific environment

--- backend/app/config/database.js
+++ backend/app/config/database.js
@@ -5,7 +5,10 @@ module.exports = ({ env }) => ({
       connector: "bookshelf",
       settings: {
         client: "postgres",
-        host: env("DATABASE_HOST", "127.0.0.1"),
+        host:
+          process.env.NODE_ENV === "production"
+            ? env("DATABASE_HOST", "postgres")
+            : env("DATABASE_HOST", "127.0.0.1"),
         port: env.int("DATABASE_PORT", 5432),
         database: env("DATABASE_NAME", "strapi"),
         username: env("DATABASE_USERNAME", "strapi"),
